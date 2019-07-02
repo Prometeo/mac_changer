15a2f545a23aae8b792cc0f03a991b20987f9a4b
Prometeo <xairi.valdivia@gmail.com>
2019-07-02 00:53:07 -0500

    First attemp changing mac address with rust

--- rust/mac_changer/src/main.rs
+++ rust/mac_changer/src/main.rs
@@ -1,5 +1,7 @@
 extern crate clap;
 use clap::{App, Arg};
+use regex::Regex;
+use std::process::Command;
 
 fn main() {
     let matches = App::new("myapp")
@@ -22,8 +24,68 @@ fn main() {
         )
         .get_matches();
 
-    let interface = matches.value_of("interface").unwrap_or("none");
-    println!("{:?}", interface)
+    let interface = String::from(matches.value_of("interface").unwrap_or("none"));
+    let mac_address = String::from(matches.value_of("mac").unwrap_or("none"));
+
+    let current_mac = get_current_mac(&interface);
+    println!("[+] current MAC for given interface: {}", current_mac);
+    let valid_mac_address = validate_mac_address(&mac_address);
+    if valid_mac_address {
+        println!("[+] Changing MAC address");
+        let result = change_mac_address(&interface, &mac_address);
+        let result_mac_address = get_current_mac(&interface);
+        println!("[+] MAC address changed to: {}", result_mac_address);
+    } else {
+        println!("[-] Please enter a valid MAC address");
+    }
+}
+
+fn change_mac_address(interface: &String, mac_address: &String) -> bool {
+    let mut mac_down = Command::new("ifconfig");
+    mac_down.arg(&interface);
+    mac_down.arg("down");
+
+    // let mut mac_change = Command::new("ifconfig");
+    // mac_change.arg(&interface);
+    // mac_change.arg("hw ether");
+    // mac_change.arg(&mac_address);
+
+    // let mut mac_up = Command::new("ifconfig");
+    // mac_up.arg(&interface);
+    // mac_up.arg("up");
+
+    return true;
+}
+
+fn validate_mac_address(mac_address: &String) -> bool {
+    let re = Regex::new(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w").unwrap();
+    let validation: bool;
+    match re.captures(&mac_address) {
+        Some(_caps) => validation = true,
+        None => validation = false,
+    }
+    return validation;
+}
+
+fn get_current_mac(interface: &String) -> String {
+    let re = Regex::new(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w").unwrap();
+    let mut cmd = Command::new("ifconfig");
+    let mut output = String::new();
+    let mut current_mac = String::new();
+    cmd.arg(interface);
+    match cmd.output() {
+        Ok(o) => unsafe {
+            output = String::from_utf8_unchecked(o.stdout);
+        },
+        Err(e) => {
+            println!("There was an error {}", e);
+        }
+    }
+    match re.captures(&output) {
+        Some(caps) => current_mac = String::from(caps.get(0).unwrap().as_str()),
+        None => current_mac = String::from("MAC not found"),
+    }
+    return current_mac;
 }
 
 //example cargo run -- -help
